857287d264949ea60ddcad953c95337790c64ea4
VincentKuo <grantor@outlook.com>
2022-09-16 17:26:35 +0800

    add ylgy.js

---
+++ QuantumultX/Rewrite/Utils/ylgy.js
@@ -0,0 +1,10 @@
+var body = $response.body
+var url = $request.url
+
+if (body) {
+    var obj = JSON.parse($response.body)
+    obj.data.map_id = 90016
+    $done({ body: JSON.stringify(obj) })
+} else {
+    $done({})
+}
